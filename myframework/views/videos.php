9ce949d22d5797d5fed9a4b3d0855bfc8d6ebe35
Tracy Spitler <tracyspitler@gmail.com>
2018-10-07 23:35:56 -0400

    Update video for Assignment 5

--- myframework/views/videos.php
+++ myframework/views/videos.php
@@ -92,6 +92,36 @@
   </div>
 </div>
 
+<div class="row mb-4">
+  <div class="col text-center">
+    <a href="" class="btn btn-lg btn-primary" data-toggle="modal" data-target="#a5">Assignment 5</a>
+  </div>
+</div>
+
+<!-- Modal 4 -->
+<div class="modal fade" id="a5" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel">Assignment 5</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h3>Assignment 5: Run Your Code!</h3>
+
+        <p>Oct. 7th</p><br>
+        <iframe src="https://www.youtube.com/embed/Fau3OH_jQFQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <p>Does not stop video on close (yet)</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 
 <?
